a2b8b86fe69892d17c390a9319921bc31bea0298
Yu Jiang Tham <yoojbruin@gmail.com>
2013-09-08 00:31:35 -0700

    Added websockets

--- strumbot.js
+++ strumbot.js
@@ -4,12 +4,56 @@
  */
 
 var five = require('johnny-five');
+var fs = require('fs');
+var http = require('http').createServer(handler);
+var io = require('socket.io').listen(http);
+
 var board, servo;
 var moveAngle;
 var beatsPerMinute = 80;
 var millisecondsPerMinute = 60000;
-var pattern = ['x x xxxx'];
+var currentPattern = 'x x x x ';
+var currentBeat = 0;
 
+// Load pattern array from text file
+var patternArray = fs.readFileSync('patterns.txt').toString().split("\n");
+
+// Initialize webserver
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error 500');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+http.listen(8080);
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('start', { hello: 'world' });
+  socket.on('click_1', function(data) {
+    currentPattern = patternArray[0];
+    console.log("1: " + currentPattern);
+  });
+  socket.on('click_2', function(data) {
+    currentPattern = patternArray[1];
+    console.log("2: " + currentPattern);
+  });
+  socket.on('click_3', function(data) {
+    currentPattern = patternArray[2];
+    console.log("3: " + currentPattern);
+  });
+  socket.on('click_4', function(data) {
+    currentPattern = patternArray[3];
+    console.log("4: " + currentPattern);
+  });
+});
+
+// Initialize johnny-five board and run
 board = new five.Board();
 board.on('ready', function() {
   servo = new five.Servo(9);
@@ -17,18 +61,26 @@ board.on('ready', function() {
   // Initial position on start
   moveAngle = 60;
   servo.move(moveAngle);
+  currentPattern = patternArray[0];
 
-  readPattern(servo, pattern);
+  this.loop(400, function () {
+    readPattern(servo, currentPattern);
+    console.log("currentPattern: " + currentPattern);
+  });
 });
 
 function readPattern(servo, pattern) {
-  for (x in pattern) {
-    if (x == 'x') {
-      strum();
-    } else {
-      // Pause;
-    }
-    sleep(millisecondsPerMinute/beatsPerMinute);
+  console.log("[Beat] " + currentBeat + ": " + pattern[currentBeat]);
+  if (pattern[currentBeat] === 'x') {
+    strum(servo);
+  } else {
+    // Pause;
+  }
+  currentBeat++;
+
+  if (currentBeat > pattern.length - 1) {
+    currentBeat = 0;
+    console.log("------------");
   }
 }
 
@@ -40,5 +92,4 @@ function strum(servo) {
     moveAngle = 60;
     servo.move(moveAngle);
   }
-  console.log("MoveAngle: " + moveAngle);
 }
